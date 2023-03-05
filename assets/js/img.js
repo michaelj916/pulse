db9e0d99610e4f7eea1cc8dd17070ee90fa75f50
michael j <michael@thinkpad-x13.localdomain>
2023-03-05 02:51:22 -0800

    Added images

--- assets/js/img.js
+++ assets/js/img.js
@@ -1,13 +1,13 @@
-const paths = ['./assets/img/processed/1.jpg', './assets/img/processed/2.jpg', './assets/img/processed/3.jpg', './assets/img/processed/4.jpg', './assets/img/processed/5.jpg', './assets/img/processed/6.jpg', './assets/img/processed/7.jpg', './assets/img/processed/8.jpg', './assets/img/processed/9.jpg', './assets/img/processed/10.jpg', './assets/img/processed/11.jpg', './assets/img/processed/12.jpg', './assets/img/processed/13.jpg', './assets/img/processed/14.jpg', './assets/img/processed/15.jpg', './assets/img/processed/16.jpg'];
-// function preload() {
-//     // loop through images in '../img/live/processed/*.jpg'
-//     let img = document.getElementById('inner-image');
-//     let path = './assets/img/processed/';
-//     for (let i = 0; i < 16; i++) {
-//         let fullPath = path.concat(i + 1, '.jpg');
-//         paths.push(fullPath);
-//     }
-// }
+const paths = [];
+const totalImg = 31;
+function populatePaths() {
+    // loop through '../img/processed/*.jpg'
+    let path = './assets/img/processed/';
+    for (let i = 0; i < totalImg; i++) {
+        let fullPath = path.concat(i + 1, '.jpg');
+        paths.push(fullPath);
+    }
+}
 
 // a simple timer
 function sleep(ms) {
@@ -20,12 +20,12 @@ async function imageSwitch() {
     while(true) {
         img.src = paths[counter];
         counter++;
-        if (counter > 15) {
+        if (counter > totalImg) {
             counter = 0;
         }
         await sleep(100);
     }
 }
 
-// preload();
+populatePaths();
 imageSwitch();
